c73aedd664d64f1266cab66360c4a97dc51ac9a4
dfsdf <ynafiai7@gmail.com>
2023-12-15 21:08:34 +0100

    Update api.sh

--- api.sh
+++ api.sh
@@ -2,5 +2,5 @@
 file=$1
 key="API_KEY"
 while read line
-do curl https://api.knoxss.pro -d target=$line -H "X-API-KEY: 9695d3f0-66eb-4948-b9af-9050131f5fd2" -s | grep PoC
+do curl https://api.knoxss.pro -d target=$line -H "X-API-KEY: bb0765d8-ddb1-4a44-b359-b826e139f11d" -s | grep PoC
 done < $file
